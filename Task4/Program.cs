1d8b8f7823fd08951a80c4161b8dfefacb8e1146
SvZor <zorinasp@yandex.ru>
2024-02-27 23:18:03 +0900

    Task4-адача с гирямиfinal

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,15 @@
 ﻿// задача с гирями
-
+// из N гирь назодим самую тяжелую
+int n = 5;
+int[] array = { 2, 1, 10, 3, 11 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.Write(max);
